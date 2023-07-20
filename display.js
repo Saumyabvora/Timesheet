7e47e216ce9757b1de11471d8552131aedaf985e
Alex Chou <achou@guidewire.com>
2023-07-20 15:29:56 -0400

    new stuff

--- display.js
+++ display.js
@@ -1,38 +1,36 @@
 function loadTimesheetData() {
-    // Updated to fetch from the Google Apps Script web app
-    fetch("https://script.google.com/a/macros/guidewire.com/s/AKfycbweNIGMJ_e3Grwk_wi0RNRsJFkdn_EYLli-3EQpvKrG8NVL1RDQjFrgTKF0W03n_aK5/exec")      
-      .then(response => response.json())
-      .then(data => updateTable(data))
-      .catch(error => console.error('Error:', error));
-  }
-  
-  function updateTable(data) {
-    const timeSlots = ["9-10", "10-11", "11-12", "12-1", "1-2", "2-3", "3-4", "4-5"];
-    const tableBody = document.querySelector("#timesheetTable tbody");
-    
-    // Clear the table body
-    tableBody.innerHTML = '';
-  
-    for (let i = 0; i < timeSlots.length; i++) {
-      const row = document.createElement('tr');
-      
-      // Create the time slot cell
-      const timeSlotCell = document.createElement('td');
-      timeSlotCell.textContent = timeSlots[i];
-      row.appendChild(timeSlotCell);
-      
-      // Create the task cells
-      for (let j = 0; j < 5; j++) {
-        const taskCell = document.createElement('td');
-        // You might need to adjust how you access your data based on its structure
-        taskCell.textContent = data[j][i] || '';  // Use the task data or an empty string if there's no data
-        row.appendChild(taskCell);
-      }
-      
-      tableBody.appendChild(row);
+  fetch('timesheet.json')
+    .then(response => response.json())
+    .then(data => updateTable(data.timesheetData))
+    .catch(error => console.error('Error:', error));
+}
+
+function updateTable(data) {
+  const timeSlots = ["9-10", "10-11", "11-12", "12-1", "1-2", "2-3", "3-4", "4-5"];
+  const tableBody = document.querySelector("#timesheetTable tbody");
+
+  // Clear the table body
+  tableBody.innerHTML = '';
+
+  for (let i = 0; i < timeSlots.length; i++) {
+    const row = document.createElement('tr');
+
+    // Create the time slot cell
+    const timeSlotCell = document.createElement('td');
+    timeSlotCell.textContent = timeSlots[i];
+    row.appendChild(timeSlotCell);
+
+    // Create the task cells
+    for (let j = 0; j < 5; j++) {
+      const taskCell = document.createElement('td');
+      const taskData = data.find(item => item.day === (j + 1) && item.timeSlot === timeSlots[i]);
+      taskCell.textContent = taskData ? taskData.activity : '';
+      row.appendChild(taskCell);
     }
+
+    tableBody.appendChild(row);
   }
-  
-  // Load the timesheet data when the page is loaded
-  loadTimesheetData();
-  
+}
+
+// Load the timesheet data when the page is loaded
+loadTimesheetData();
--- main.js
+++ main.js
@@ -1,38 +1,41 @@
 document.getElementById('timesheetForm').addEventListener('submit', function(e) {
     e.preventDefault();
-
+  
     // Show the loading text
     document.getElementById('loading').style.display = 'block';
-
+  
     var day = document.getElementById('day').value;
     var timeSlot = document.getElementById('timeSlot').value;
     var activity = document.getElementById('activity').value;
-
+  
     var data = {
-        "day": day,
-        "timeSlot": timeSlot,
-        "activity": activity
-    };  
-
-    fetch('https://script.google.com/a/macros/guidewire.com/s/AKfycbweNIGMJ_e3Grwk_wi0RNRsJFkdn_EYLli-3EQpvKrG8NVL1RDQjFrgTKF0W03n_aK5/exec', {
-        method: 'POST',
-        mode: 'no-cors',
-        headers: {
+      "day": parseInt(day),
+      "timeSlot": timeSlot,
+      "activity": activity
+    };
+  
+    fetch('timesheet.json')
+      .then(response => response.json())
+      .then(timesheetData => {
+        timesheetData.push(data);
+        return fetch('timesheet.json', {
+          method: 'PUT',
+          headers: {
             'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data),
-    })
-    .then(response => response.text())
-    .then(text => {
-        console.log(text);
+          },
+          body: JSON.stringify({ timesheetData })
+        });
+      })
+      .then(() => {
         // Hide the loading text
         document.getElementById('loading').style.display = 'none';
         // Redirect to the display page
         window.location.href = 'display.html';
-    })
-    .catch(error => {
+      })
+      .catch(error => {
         console.error('Error:', error);
         // Hide the loading text
         document.getElementById('loading').style.display = 'none';
-    });
-});
+      });
+  });
+  
