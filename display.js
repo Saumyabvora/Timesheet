53dd5ddecbefca3e97469f93a1ef2ed73a2b147d
svora <svora@guidewire.com>
2023-07-20 16:00:36 -0400

    PAINN

--- display.js
+++ display.js
@@ -1,35 +1,51 @@
 function loadTimesheetData() {
-  fetch('timesheet.json')
-    .then(response => response.json())
-    .then(data => updateTable(data.timesheetData))
-    .catch(error => console.error('Error:', error));
+  fetch("C:\\Users\\svora\\Timesheet\\timesheet.json")
+    .then((response) => response.json())
+    .then((data) => updateTable(data))
+    .catch((error) => console.error("Error:", error));
 }
 
 function updateTable(data) {
-  const timeSlots = ["9-10", "10-11", "11-12", "12-1", "1-2", "2-3", "3-4", "4-5"];
+  const timeSlots = [
+    "9-10",
+    "10-11",
+    "11-12",
+    "12-1",
+    "1-2",
+    "2-3",
+    "3-4",
+    "4-5",
+  ];
   const tableBody = document.querySelector("#timesheetTable tbody");
 
   // Clear the table body
-  tableBody.innerHTML = '';
+  tableBody.innerHTML = "";
 
   for (let i = 0; i < timeSlots.length; i++) {
-    const row = document.createElement('tr');
+    const row = document.createElement("tr");
 
     // Create the time slot cell
-    const timeSlotCell = document.createElement('td');
+    const timeSlotCell = document.createElement("td");
     timeSlotCell.textContent = timeSlots[i];
     row.appendChild(timeSlotCell);
 
-    // Create the task cells
+    // Create the task cells for each day
     for (let j = 0; j < 5; j++) {
-      const taskCell = document.createElement('td');
-      const taskData = data.find(item => item.day === (j + 1) && item.timeSlot === timeSlots[i]);
-      taskCell.textContent = taskData ? taskData.activity : '';
+      const taskCell = document.createElement("td");
+      const activity = getActivity(data, timeSlots[i], j);
+      taskCell.textContent = activity || "";
       row.appendChild(taskCell);
     }
 
     tableBody.appendChild(row);
   }
+}
+
+function getActivity(data, timeSlot, dayIndex) {
+  const entry = data.find(
+    (item) => item.timeSlot === timeSlot && item.dayIndex === dayIndex
+  );
+  return entry ? entry.activity : "";
 }
 
 // Load the timesheet data when the page is loaded
--- main.js
+++ main.js
@@ -1,41 +1,44 @@
-document.getElementById('timesheetForm').addEventListener('submit', function(e) {
+document
+  .getElementById("timesheetForm")
+  .addEventListener("submit", function (e) {
     e.preventDefault();
-  
+
     // Show the loading text
-    document.getElementById('loading').style.display = 'block';
-  
-    var day = document.getElementById('day').value;
-    var timeSlot = document.getElementById('timeSlot').value;
-    var activity = document.getElementById('activity').value;
-  
+    document.getElementById("loading").style.display = "block";
+
+    var day = document.getElementById("day").value;
+    var timeSlot = document.getElementById("timeSlot").value;
+    var activity = document.getElementById("activity").value;
+
     var data = {
-      "day": parseInt(day),
-      "timeSlot": timeSlot,
-      "activity": activity
+      day: day,
+      timeSlot: timeSlot,
+      activity: activity,
     };
-  
-    fetch('timesheet.json')
-      .then(response => response.json())
-      .then(timesheetData => {
-        timesheetData.push(data);
-        return fetch('timesheet.json', {
-          method: 'PUT',
+
+    fetch("C:\\Users\\svora\\Timesheet\\timesheet.json", {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((jsonData) => {
+        jsonData.push(data);
+        return fetch("C:\\Users\\svora\\Timesheet\\timesheet.json", {
+          method: "PUT",
           headers: {
-            'Content-Type': 'application/json'
+            "Content-Type": "application/json",
           },
-          body: JSON.stringify({ timesheetData })
+          body: JSON.stringify(jsonData),
         });
       })
       .then(() => {
         // Hide the loading text
-        document.getElementById('loading').style.display = 'none';
+        document.getElementById("loading").style.display = "none";
         // Redirect to the display page
-        window.location.href = 'display.html';
+        window.location.href = "display.html";
       })
-      .catch(error => {
-        console.error('Error:', error);
+      .catch((error) => {
+        console.error("Error:", error);
         // Hide the loading text
-        document.getElementById('loading').style.display = 'none';
+        document.getElementById("loading").style.display = "none";
       });
   });
-  
